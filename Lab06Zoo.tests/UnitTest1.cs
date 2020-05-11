9f41437f1079311dff86387d74600f452855ecb2
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-11 16:11:28 -0500

    added a couple tests for lake sturgeon

--- Lab06Zoo.tests/UnitTest1.cs
+++ Lab06Zoo.tests/UnitTest1.cs
@@ -5,6 +5,35 @@ namespace Lab06Zoo.tests
 {
     public class UnitTest1
     {
+        [Fact]
+        public void WhatSoundDoesSturgeonMake()
+        {
+            //arrange
+            Animal steve = new LakeSturgeon();
+            string expected = "I can make low-frequence 'sturgeon thunder'!";
+
+            //act
+            string result = steve.MakeSound();
+
+            //Assert
+            Assert.Equal(expected, result);
+
+        }
+        [Fact]
+        public void Fish_Sturgeon_Can_Swim()
+        {
+            //arrange
+            Fish bob = new LakeSturgeon();
+            string expected = "Just keep swimming...";
+
+            //act
+            //Couldn't use bob.Swim, but had to use Fish.Swim()?
+            string result = Fish.Swim();
+
+            //Assert
+            Assert.Equal(expected, result);
+
+        }
       
     }
 }

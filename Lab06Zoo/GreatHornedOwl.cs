3419f6da5da4789cbaa813291c7d5c324050613e
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-11 19:22:27 -0500

    made classs for Great horned owl

---
+++ Lab06Zoo/GreatHornedOwl.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab06Zoo
+{
+    public class GreatHornedOwl : Bird
+    {
+        public override bool CanThisAnimalFly => true;
+
+        public override int Weight => 3;
+
+        public override string Eat()
+        {
+            return "I eat rodents, hares, and squirrels";
+        }
+
+        public override string MakeSound()
+        {
+            return "hoo-h'HOO-hoo-hoo";
+        }
+        public override string Sleep()
+        {
+            return "I sleep mostly during the day";
+        }
+    }
+}

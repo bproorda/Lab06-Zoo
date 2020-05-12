201b0624e11ae8fbf675c43fa6ea9f5e089ebb74
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-11 19:35:33 -0500

    Added class for Loon

---
+++ Lab06Zoo/Loon.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab06Zoo
+{
+    public class Loon : Bird
+    {
+        public override bool CanThisAnimalFly => true;
+
+        public override int Weight => 4;
+
+        public override string Eat()
+        {
+            return "I eat mostly fish, with the occasional frog";
+        }
+
+        public override string MakeSound()
+        {
+            return "Not even going to try this one: https://www.cornell.edu/video/understanding-loons-6-vocalizations";
+        }
+    }
+}

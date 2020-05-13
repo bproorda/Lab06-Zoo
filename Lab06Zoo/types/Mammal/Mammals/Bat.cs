41622889c486ee9d182071fddb84770483773f57
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-12 19:38:09 -0500

    Added new animal class, Bat!

---
+++ Lab06Zoo/types/Mammal/Mammals/Bat.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab06Zoo
+{
+    public class Bat : Mammal
+    {
+        public override int Weight => 1;
+
+        public override string Eat()
+        {
+            return "I love to eat bugs as I fly through the air!";
+        }
+
+        public override string MakeSound()
+        {
+            return "I use my voice to see in the dark!";
+        }
+
+        public override string Sleep()
+        {
+            return "I like to hide in caves and other dark places during the day;"
+        }
+    }
+    
+
+}
